837516265e781162a2cd7772a00c9df79283c5f2
Daniel <castrokevin312@gmail.com>
2025-11-06 23:09:36 -0500

    media_local

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,11 +70,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 
 ]
 
