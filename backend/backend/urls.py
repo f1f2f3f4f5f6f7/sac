12a0ec3559e24684494d65a0efd67d8db347ffe3
Villafu <santiagogv0506@gmail.com>
2025-12-12 21:05:24 -0500

    prestamo funcionando

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')), 
     path('api/dataImport/', include('dataImport.urls')),
     path('api/movements/', include('movements.urls')),
+    path('api/Prestamo/', include('Prestamo.urls')),
     path('api/editarElemento/', include('editarElemento.urls'))
 ]
 
